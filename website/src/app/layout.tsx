013cc86eaa3105b244bb43a1a60e12c1e51da492
sohaibqasem <sohaib_alqasem@yahoo.com>
2026-08-26 07:32:03 +0300

    Fix: add static search for static export compatibility

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -26,7 +26,15 @@ export default function Layout({ children }: LayoutProps<'/'>) {
       suppressHydrationWarning
     >
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider
+          search={{
+            options: {
+              type: 'static',
+            },
+          }}
+        >
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
